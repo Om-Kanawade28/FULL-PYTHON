febae211c0c5efde7b999547d4d05f65b50c4c96
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-06-09 07:44:54 +0530

    Defining Functions

---
+++ 04_Functions/01_Defining_Functions.py
@@ -0,0 +1,20 @@
+# Defining Functions in Python:
+
+# Functions are reusable blocks of code that perform specific tasks. They help you organize your code, make it more readable, and avoid repetition.
+
+# 1. Basic Function Definition:
+
+#   - Use the `def` keyword followed by the function name and parentheses.
+#   - The function body is indented.
+#   - The `return` statement specifies the value the function returns (optional).
+
+#   - Example:
+
+def greet(name):
+    """Greets the user with a personalized message."""
+    print(f"Hello, {name}!")
+
+greet("Alice")
+#output:-
+# Hello, Alice!
+
